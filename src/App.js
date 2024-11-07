a8031d0bdc9d56207ca1254aaf6b9f7eb949eb5f
pawneshkumar1 <98223876+pawneshshakya@users.noreply.github.com>
2024-11-07 11:04:23 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "./App.css";
+import About from "./components/about/About";
+import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
+import Home from "./components/home/Home";
+import Work from "./components/portfolio/Work";
+import Qualification from "./components/qualification/Qualification";
+import ScrollUp from "./components/scrollup/ScrollUp";
+import Services from "./components/services/Services";
+import Skills from "./components/skills/Skills";
+import Testimonials from "./components/testimonials/Testimonials";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main className="main">
+        <Home />
+        <About />
+        <Skills />
+        <Services />
+        <Qualification />
+        <Testimonials />
+        <Work />
+        <Contact />
+      </main>
+      <Footer />
+      <ScrollUp />
+    </>
+  );
+}
+
+export default App;
